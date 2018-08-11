265d3d15501ab4a8241ab53302423a756d897513
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-11 23:05:39 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    # sentencesCount = self.split(/[?.!]/).reject!{|item| item.empty?}.size
+    sentencesCount = self.split(/[?.!]/).reject!{|item| item.empty?}.size
     puts self.split(/[?.!]/).reject!{|item| item.empty?}.size
     if self.split(/[?.!]/).size == 0
       return 0 
